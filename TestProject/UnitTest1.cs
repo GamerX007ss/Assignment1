5bb667798787878cf52cd3bec97f304e0cabe3ea
GamerX007ss <pranav007ss@gmail.com>
2025-02-04 14:35:31 -0500

    added first four test cases

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,16 +1,40 @@
+using System.Runtime.CompilerServices;
+using Assignment1;
+using NUnit.Framework;
 namespace TestProject
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        
+        //Product ID
+        [Test]
+        public void ProdIdIntializationTest()
         {
+            var product = new Product(100, "Test Product", 100, 50);
+            Assert.That(product.ProdId, Is.EqualTo(100));
+            
+        }
+        [Test]
+        public void ProdIdMinimumTest()
+        {
+            var product = new Product(5, "Test Product", 100, 50);
+            Assert.That(product.ProdId, Is.EqualTo(5));
+
+        }
+        [Test]
+        public void ProdIdMaximumTest()
+        {
+            var product = new Product(50000, "Test Product", 100, 50);
+            Assert.That(product.ProdId, Is.EqualTo(50000));
+
         }
 
+        //Item Price
         [Test]
-        public void Test1()
+        public void PriceIntialization()
         {
-            Assert.Pass();
+            var product = new Product(100, "Test Product", 100, 50);
+            Assert.That(product.ItemPrice, Is.EqualTo(100));
         }
     }
 }
--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b774e4c2f78f24654c75a7560853def88a4e51f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
